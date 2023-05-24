c5764e22455d4bface116e319daff3eaa571b98b
Naresh Gupta <nareshgupta0899@gmail.com>
2023-05-25 00:17:54 +0530

    bug fix side-menu

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react'
 import {AiOutlineMenu} from 'react-icons/ai'
 import menu from './sidebar-config'
 import { useNavigate } from 'react-router-dom'
-import ProfileUserDetails from '../profile/ProfileUserDetails'
-import Profile from '../../pages/profile/Profile'
+import "./sidebar.css"
 function Sidebar() {
-const[active,setActive]=useState("Home")
+const[active,setActive]=useState("")
   const navigate=useNavigate();
 
   const handleClick=(title,e)=>{
@@ -29,8 +28,8 @@ const[active,setActive]=useState("Home")
   }
 
   return (
-    <div className='w-56 h-[100vh] border-r-[1px] border-gray-400'>
-      <div >
+    <div className='border-r-[1px] h-[100%]  '>
+      <div className='border-gray-400  side-menu sticky top-0 w-56'>
         <div className='pt-10'>
           <img className='w-40 cursor-pointer px-5 ' src='https://i.imgur.com/zqpwkLQ.png' alt='' />
         </div>
@@ -41,10 +40,10 @@ const[active,setActive]=useState("Home")
         <p className={item.title===active?'font-semibold':'font-normal'}>{item.title}</p>
         </div>)}
         </div>
-      </div>
       <div className='flex items-center cursor-pointer pl-5 mt-24 text-lg '>
       <AiOutlineMenu className='font-extrabold w-5  text-[26px]' />
       <p className='ml-5'>More</p>
+      </div>
       </div>
       
     </div>
--- src/pages/router/Router.jsx
+++ src/pages/router/Router.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import Sidebar from '../../component/sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../home/Home'
-import Profile from '../profile/Profile'
+import React from "react";
+import Sidebar from "../../component/sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Home from "../home/Home";
+import Profile from "../profile/Profile";
 
 function Router() {
   return (
     <div>
-        <div className='flex ' >
-            <div className=''>
-                <Sidebar />
-            </div>
-           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/username' element={<Profile />} />
-           </Routes>
+      <div className="flex">
+        <div className="">
+
+        <Sidebar />
         </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/username" element={<Profile />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Router
+export default Router;
